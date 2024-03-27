e80cc48066be9d7b6886ec85746a3531357e3788
petershiyili <159577853+petershiyili@users.noreply.github.com>
2024-03-27 03:31:06 -0400

    Add files via upload

---
+++ AboutUs.js
@@ -0,0 +1,63 @@
+import React from 'react';
+
+const About = () => {
+  return (
+    <div>
+      <h2>ABOUT PTR IMMIGRATION</h2>
+      <p>Canadian Immigration Consultants</p>
+      <p>PTR Immigration Services is an immigration organization that offers a wide range of immigration services to companies and individuals.</p>
+      <p>Clients at PTR Immigration Services receive the professionalism, useful guidance, and availability of their immigration consultant that are crucial to attaining their objective.</p>
+
+      <h3>Real Time Status</h3>
+      <p>Our team assures that our clients are well-informed of their application.</p>
+
+      <h3>Experienced Team</h3>
+      <p>A team that is dedicated to achieving the highest level of customer satisfaction.</p>
+
+      <h2>Services We Offer</h2>
+      <p>We believe in giving you the choice on how you want to take your journey to Canada.</p>
+      <p>We provide you the ability to choose your level of service to fit your budget while providing you the ability to access professional advice and guidance when and how you need it.</p>
+
+      <h3>Sponsorship</h3>
+      <p>Family reunification and reconnecting loved ones with their families is a major focus for Canada. If you or a family member are Canadian residents or citizens, you may be eligible to be sponsored or sponsor a loved one.</p>
+
+      <h3>READ MORE</h3>
+      <p>Study Permit</p>
+      <p>Studying in Canada is an exciting and rewarding experience for many students around the world. The main intake is in September each year, though many colleges and universities have a smaller intake in January or summer term.</p>
+
+      <p>Citizenship</p>
+      <p>Canada has one of the most positive immigration policies in the world and welcomes more than 200,000 new immigrants each year. Getting the PR is the first step towards obtaining a citizenship in Canada.</p>
+
+      <p>Visitor Visa</p>
+      <p>Every year, Canada welcomes nearly 50 million people from different countries. You can visit Canada as a tourist, to see family and friends, or for business.</p>
+
+      <p>Super Visa</p>
+      <p>The Super Visa Program allows parents and grandparents to come to Canada as long-term visitors.</p>
+
+      <p>Express Entry</p>
+      <p>Express Entry applicants receive priority processing and increased accessibility for candidates from a broader range of occupations.</p>
+
+      <p><a href="#">VIEW ALL SERVICES</a></p>
+
+      <h2>Why choose us?</h2>
+      <p>At PTR Immigration we think that time is extremely important in our life, as it helps us structure our daily activities, and be more organized and productive. Unlike many other Immigration Consultants we offer innovative online services from the city of Toronto to help you save your time and money. With our professional guidance and latest technologies you will be always a few steps closer to your Canadian Dream.</p>
+
+      <h3>Personalized Approach</h3>
+      <p>At PTR Immigration, we develop effective immigration strategy for each client individually. Our team works hard on each and every case to maximize your chances of getting approved.</p>
+
+      <h3>Transparent Pricing</h3>
+      <p>Enhanced Flexibility</p>
+
+      <h2>ABOUT THE FOUNDER</h2>
+      <p>Meet your Immigration Consultant</p>
+
+      <p>Pooja Singh</p>
+      <p>Pooja Singh, the principal of PTR Immigration Services, is a Licensed Immigration Consultant.</p>
+      <p>Being a specialist in Citizenship and Immigration Law, she represents a broad range of clientele, including individuals, families, and businesses, providing knowledgeable and efficient legal services in immigration matters.</p>
+      <p>She is an enthusiastic communicator with over 10 years of experience in Talent Management, she is consistently demonstrating excellent business acumen by focusing on the bottom-line without sacrificing integrity, quality, and outstanding customer service.</p>
+      <p>She also mentors’ newcomers to Canada through TRIEC.</p>
+    </div>
+  );
+};
+
+export default About;
